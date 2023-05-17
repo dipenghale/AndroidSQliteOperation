fdc90fa607fb6897a0e946c41c4212b0cf71b38f
Evelynn Ghale <eveghale@Eve-MacBook-Pro.mshome.net>
2023-05-17 13:27:45 +0545

    android sqlite project push

---
+++ app/src/main/java/com/example/androidsqlite/ContactsDB.java
@@ -0,0 +1,95 @@
+package com.example.androidsqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsDB {
+    public static final String Key_RowId = "_id";
+    public static final String Key_Name = "person_name";
+    public static final String Key_Cell = "_cell";
+
+    private final String Database_Name = "ContactsDB"; //Database Name
+    private final String Database_Table = "ContactsTable";
+    private final int Database_Version = 1;
+
+    private DBHelper ourHelper;
+    private final Context ourContext;
+    private SQLiteDatabase ourdatabase;
+
+    public ContactsDB(Context context) {
+        ourContext = context;
+    }
+
+
+
+    private class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context) {
+            super(context, Database_Name, null, Database_Version);
+
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String sqlcode = "CREATE TABLE ContactsTable(_id INTEGER PRIMARY KEY AUTOINCREMENT, person_name TEXT NOT NULL, _cell TEXT NOT NULL);";
+            db.execSQL(sqlcode);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+            db.execSQL("DROP TABLE IF EXISTS " + Database_Table);
+            onCreate(db);
+        }
+    }
+    public ContactsDB open() throws SQLException {
+        ourHelper = new DBHelper(ourContext);
+        ourdatabase = ourHelper.getWritableDatabase();
+        return this;
+    }
+    public void close() {
+        ourHelper.close();
+    }
+    public long creat(String name, String cell) {
+        ContentValues cv = new ContentValues();
+        cv.put(Key_Name, name);
+        cv.put(Key_Cell, cell);
+        return ourdatabase.insert(Database_Table, null, cv);
+    }
+    public String returndata() {
+        String[] column = new String[] {
+                Key_RowId,
+                Key_Name,
+                Key_Cell
+        };
+        Cursor c = ourdatabase.rawQuery("SELECT * FROM "+Database_Table,null);
+        String resu = "";
+        //int irowid = c.getInt(0);
+
+        while (c.moveToNext()) {
+
+            resu = resu + c.getString(1) + ":" + c.getString(2) + "\n";
+        }
+        //c.close();
+        return resu;
+    }
+
+    public long deleteEnter(String rowId) {
+        return ourdatabase.delete(Database_Table, Key_RowId + "=?", new String[] {
+                rowId
+        });
+    }
+
+    public long update(String rowId, String cell, String name) {
+        ContentValues cu = new ContentValues();
+        cu.put(Key_Name, name);
+        cu.put(Key_Cell, cell);
+        return ourdatabase.update(Database_Table, cu, Key_RowId + "=?", new String[] {
+                rowId
+        });
+    }
+}
---
+++ app/src/main/java/com/example/androidsqlite/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.androidsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etname, etcell;
+    Button bshow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etname = findViewById(R.id.etna);
+        etcell = findViewById(R.id.etcell);
+        bshow = findViewById(R.id.bshow);
+        bshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Data.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    public void submit(View v) {
+        String name = etname.getText()
+                .toString()
+                .trim();
+        String cell = etcell.getText()
+                .toString()
+                .trim();
+        try {
+            ContactsDB db = new ContactsDB(this);
+            db.open();
+            db.creat(name, cell);
+            db.close();
+            Toast.makeText(MainActivity.this, "Successfully saved ", Toast.LENGTH_LONG)
+                    .show();
+            etname.setText("");
+            etcell.setText("");
+        } catch (SQLException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    public void edit(View v) {
+        try {
+            ContactsDB db = new ContactsDB(this);
+            db.open();
+            db.update("1", "John", "24334421");
+            db.close();
+            Toast.makeText(MainActivity.this, "Successfully updated", Toast.LENGTH_LONG)
+                    .show();
+        } catch (SQLException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    public void delete(View v) {
+        try {
+            ContactsDB db = new ContactsDB(this);
+            db.open();
+            db.deleteEnter("1");
+            Toast.makeText(MainActivity.this, "Successfully delete", Toast.LENGTH_LONG)
+                    .show();
+            db.close();
+        } catch (SQLException e) {
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+}
